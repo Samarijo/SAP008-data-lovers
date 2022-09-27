f4ed5ece3dc7dac15d9d43a0852a88b9df7f65f8
samarijo <sabrinamariano1994@hotmail.com>
2022-09-26 22:44:49 -0300

    Updates

--- main.js
+++ main.js
@@ -1,28 +1,51 @@
 
 import data from './data/harrypotter/data.js';
 
-import {filterBySearch, filterHouse, sortByName} from './data.js';
+import {filterBySearch, filterHouse, sortByName, calculoAgregado} from './data.js';
 
 const personagens = data.characters;
 
 function printCards(hp) {
-    document.getElementById('characters').innerHTML = hp.map((personagem) => 
-        `  
-            <div>
-                <p>Name: ${personagem.name}</p>          
-            </div>
-        `    
-    ).join("")
+    document.getElementById('characters').innerHTML = ``
+    hp.forEach((personagem) => {
+        const container = document.createElement("div")
+        container.innerHTML = `
+                                <p id="${personagem.id}">Name: ${personagem.name}</p>
+                              `
+        container.addEventListener("click", ()=> modal(personagem))
+        document.getElementById('characters').appendChild(container)
+    })
 }
 
 printCards(personagens)
 
-
+function modal(personagem){
+    document.getElementById("modal-click").style.display = "block"
+    document.getElementById("modal-click").innerHTML = `  
+    <div>
+        <p>Name: ${personagem.name}</p>
+        <p>Birth: ${personagem.birth}</p>
+        <p>Death: ${personagem.death}</p>
+        <p>Species: ${personagem.species}</p>
+        <p>Ancestry: ${personagem.ancestry}</p>
+        <p>Gender: ${personagem.gender}</p>
+        <p>Patronus: ${personagem.patronus}</p>
+        <p>House: ${personagem.house}</p>
+        <p>Books featured in: ${personagem.books_featured_in}</p>
+    </div>
+`
+    const fechar = document.createElement("div")
+    fechar.innerHTML = `<p id="fechar-modal"> X </p>`
+    fechar.addEventListener("click", )
+}
 
 function filterByHouse(e) {
     const houseSelec = e.target.value;
     const filter = filterHouse(personagens, houseSelec)
     printCards(filter)
+
+    const quantidade = calculoAgregado(personagens,"house", houseSelec)
+    statistic.innerHTML = printStatistic(quantidade, houseSelec)
 }
 
 const houseFilter = document.getElementById("inputFilter")
@@ -30,6 +53,7 @@ houseFilter.addEventListener ("change", filterByHouse)
 
 function sortCharacters (e) {
     const order = e.target.value
+    document.getElementById('houses-members').innerHTML = ``
     const sorted = sortByName(personagens, order)
     printCards(sorted)
 }
@@ -39,6 +63,7 @@ sortSelect.addEventListener("change", sortCharacters)
 
 function search(e) {
     const nameInserido = e.target.value
+    document.getElementById('houses-members').innerHTML = ``
     const busca = filterBySearch(personagens, nameInserido)
     printCards(busca)
 
@@ -46,4 +71,14 @@ function search(e) {
 
 const searchName = document.getElementById("input-search")
 searchName.addEventListener("keyup", search)
+
+const statistic = document.getElementById("houses-members")
+
+function printStatistic(total, house) {
+    return `
+             <p>House ${house} has ${total} members</p>
+
+            `
+}
+
 
